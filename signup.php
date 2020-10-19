8e4b2dc39f596b8d81da6ad22f2623ec0d6cd74b
Faizan Parabtani <faizan11911@gmail.com>
2020-10-19 22:51:56 +0530

    new

--- signup.php
+++ signup.php
@@ -6,13 +6,25 @@ include 'dbcon.php';
 $username = $_POST['username'];
 $email = $_POST['email'];
 $password = $_POST['password'];
+$sql = "SELECT * FROM Profile WHERE UserName = '$username'";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_array($result);
+$count = mysqli_num_rows($result);
+if ($count == 1) {
+   header('location: signup.html');
+   echo '<script>alert("Username Already Exists")</script>';
+}
+else
+{
+	$sql1 = "INSERT INTO Profile (UserName, Mail, Password) values ('$username', '$email', '$password')";
 
-$sql = "INSERT INTO Profile (UserName, Mail, Password) values ('$username', '$email', '$password')";
-
-if (mysqli_query($conn, $sql)) {
+   if (mysqli_query($conn, $sql1)) {
     echo '<script>alert("Registration Successful.")</script>';
     header('location: login.html');
- } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
- }
+} else {
+    echo "Error: " . $sql1 . "<br>" . mysqli_error($conn);
+ }	
+}
+
+
 ?>
